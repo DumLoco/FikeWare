b2c8155db653ac5636c534ec57501abdeba8ef27
Vait0rIvo <ivox2312@gmail.com>
2021-03-14 06:01:45 -0300

    sdfsñds

--- js/FuncionesBotonPlanes.js
+++ js/FuncionesBotonPlanes.js
@@ -11,15 +11,20 @@ function OcultarMostrar(objetoMostrarOcultarId) {
 function aumentarReducirTamaño(objetoIdAumentarTamaño, alturaNueva) {
     const objetoAumentar = document.getElementById(objetoIdAumentarTamaño);
 
-    if (objetoAumentar.clientHeight > 1000){
-        if (screen.width < 1700){
-            objetoAumentar.style.height = "770px";
-        }else{
+    if (objetoAumentar.clientHeight > 1000){ //Verifca Tamaño del objeto
+        if (screen.width > 1700){ //Verifca Tamaño de pantalla 1920
             objetoAumentar.style.height = "700px";
+        }else{
+            if(screen.width > 1200){ //1366
+                objetoAumentar.style.height = "770px";
+            }
+            if(screen.width < 550){ //450
+                objetoAumentar.style.height = "1270px";
+            }       
         }
-    }else if (objetoAumentar.clientHeight < 1000){
+    }else if (objetoAumentar.clientHeight < 1000){ //Verifca Tamaño del objeto
         if (screen.width < 1700){
-            objetoAumentar.style.height = "1250px";
+            objetoAumentar.style.height = "1400px";
         }else{
             objetoAumentar.style.height = "1250px";
         }
@@ -30,17 +35,17 @@ function aumentarReducirTamaño(objetoIdAumentarTamaño, alturaNueva) {
 function cambiarPosicionElemento(elementoIdCambiarPosicion, marginTopNuevo, marginTopOriginal){
     const elementoCambiarPosicion = document.getElementById(elementoIdCambiarPosicion);
     
-    if (screen.width > 1600){
+    if (screen.width > 1400){
         if (elementoCambiarPosicion.style.marginTop != marginTopNuevo){
             elementoCambiarPosicion.style.marginTop = marginTopNuevo
         }else{
             elementoCambiarPosicion.style.marginTop = marginTopOriginal
         }
-    }else if (screen.width < 1600){
-        if (elementoCambiarPosicion.style.marginTop != '10px'){
-            elementoCambiarPosicion.style.marginTop = '10px'
+    }else if (screen.width < 1700){
+        if (elementoCambiarPosicion.style.marginTop != '10px;'){
+            elementoCambiarPosicion.style.marginTop = '10px;'
         }else{
-            elementoCambiarPosicion.style.marginTop = '10px'
+            elementoCambiarPosicion.style.marginTop = '110px;'
         }
     }
     
